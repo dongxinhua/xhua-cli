602305bd8ab5a91f62b76ad05ee638f74ca4cca7
dongxinhua <798616582@qq.com>
2022-01-25 20:44:11 +0800

    feat: version and help info

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 #! /usr/bin/env node
+
+const { program } = require('commander')
+
+const setOptions = require('./lib/core/options.js')
+
+// 打印版本
+program.version(require("./package.json").version)
+
+// 配置help信息
+setOptions()
+
+program.parse(process.argv)
