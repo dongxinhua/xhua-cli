cdef9ed76f37b56969563156ef8cca770c6cfbfd
dongxinhua <798616582@qq.com>
2022-01-25 21:03:26 +0800

    feat: 拉取模板并下载

--- index.js
+++ index.js
@@ -3,11 +3,15 @@
 const { program } = require('commander')
 
 const setOptions = require('./lib/core/options.js')
+const createProject = require('./lib/core/create.js')
 
 // 打印版本
 program.version(require("./package.json").version)
 
 // 配置help信息
 setOptions()
+
+// 创建项目
+createProject()
 
 program.parse(process.argv)
